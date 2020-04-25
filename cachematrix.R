e732305b9615ab2db3deea8e44371e7ee634cd3d
gcmlw <62331772+gcmlw@users.noreply.github.com>
2020-04-25 16:13:31 +0800

    First cut of the Programming Assignment for Course 2 Week 3

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,29 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        setminverse <- function(minverse) m <<- minverse
+        getminverse <- function() m
+        list(set = set, mat  =  x,
+             setminverse = setminverse,
+             getminverse = getminverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getminverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x[["mat"]]
+        m <- solve(data)
+        x$setminverse(m)
+        m
 }
