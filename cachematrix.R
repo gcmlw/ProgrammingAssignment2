bd502a256a9aa1a9c88ed02d4e197b5487ddae94
gcmlw <62331772+gcmlw@users.noreply.github.com>
2020-04-25 16:52:47 +0800

    Add comments for the .R and each functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,9 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+# The 2 functions here provide a mechanism to cache the inverse of a matrix. Calculating
+# the inverse of a matrix can be a computational intensive operation. The use of cache
+# helps to speed up the efficiency of the calculation.
 
+## Creates a list containing the matrix and the functions to get/set the matrix inverse
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
         set <- function(y) {
@@ -17,8 +18,8 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
-
+## Takes in the results from makeCacheMatrix() and determine whether cached data is available.
+## If cached data is available, it takes cached data. If not, it calculate and set as cached
 cacheSolve <- function(x, ...) {
         m <- x$getminverse()
         if(!is.null(m)) {
